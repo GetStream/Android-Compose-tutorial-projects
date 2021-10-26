5c130b4fbfc57bb66d59f0800d32e0b438c1d4c7
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2021-10-26 14:14:27 +0300

    Initial Commit

---
+++ LiveStreamSwiftUIKit/LiveStreamSwiftUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveStreamSwiftUIKitApp.swift
+//  LiveStreamSwiftUIKit
+//
+//  Created by amos.gyamfi@getstream.io on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct LiveStreamSwiftUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
